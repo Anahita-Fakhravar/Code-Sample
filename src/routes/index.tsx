8ad6d61c05d26da7f74b67bf7596c7e8047a4dfc
Anna <anna.fakhravar@itshere.com>
2026-04-25 13:06:34 +0330

    feat: use modern eslint config cs-2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-export const Route = createFileRoute('/')({
-  component: Index,
-});
+import { IndexPage } from '../pages/index-page';
 
-function Index() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  );
-}
+export const Route = createFileRoute('/')({
+  component: IndexPage,
+});
