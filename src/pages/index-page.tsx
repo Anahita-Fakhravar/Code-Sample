1539ad1560cc97a248c18d10795129f2091f5ec3
Anna <anna.fakhravar@itshere.com>
2026-04-25 17:28:44 +0330

    feat: test github action functionality cs-4

--- src/pages/index-page.tsx
+++ src/pages/index-page.tsx
@@ -2,7 +2,8 @@ export function IndexPage() {
   const test = 123;
   return (
     <div className="p-2">
-      <h3>Welcome Home! test: {test}</h3>
+      <h3>Welcome Home!</h3>
+      <p>{test}</p>
     </div>
   );
 }
